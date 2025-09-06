dddb69468299158826d087e752d3129846c6c96a
Onu Wisdom <onuwisdom2000@gmail.com>
2025-09-06 22:29:04 +0100

    style changes

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,9 @@
 const Map = () => {
 	return (
-		<div id="map" class="h-[calc(100vh-60px)] w-full z-[1] relative"></div>
+		<div
+			id="map"
+			className="h-[calc(100vh-160px)] sm:h-[calc(100vh-60px)] w-full z-[1] relative"
+		></div>
 	);
 };
 export default Map;
